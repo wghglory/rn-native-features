f5f2d79140cd50da3b80269925bf4a157f012c99
Guanghui Wang <guanghui-wang@foxmail.com>
2022-06-03 22:06:53 +0800

    feat: fetch places from db

--- models/place.js
+++ models/place.js
@@ -1,9 +1,10 @@
 export class Place {
-  constructor(title, imageUri, location) {
+  constructor(title, imageUri, location, id) {
     this.title = title;
     this.imageUri = imageUri;
     this.address = location.address;
     this.location = { lat: location.lat, lng: location.lng }; // { lat: 0.141241, lng: 127.121 }
-    this.id = new Date().toString() + Math.random().toString();
+    this.id = id;
+    // this.id = new Date().toString() + Math.random().toString();
   }
 }
--- util/database.js
+++ util/database.js
@@ -1,5 +1,7 @@
 import * as SQLite from 'expo-sqlite';
 
+import { Place } from '../models/place';
+
 const database = SQLite.openDatabase('places.db');
 
 export function init() {
@@ -17,6 +19,66 @@ export function init() {
         [],
         () => {
           resolve();
+        },
+        (_, error) => {
+          reject(error);
+        },
+      );
+    });
+  });
+
+  return promise;
+}
+
+export function insertPlace(place) {
+  const promise = new Promise((resolve, reject) => {
+    database.transaction(tx => {
+      tx.executeSql(
+        `INSERT INTO places (title, imageUri, address, lat, lng) VALUES (?, ?, ?, ?, ?)`,
+        [
+          place.title,
+          place.imageUri,
+          place.address,
+          place.location.lat,
+          place.location.lng,
+        ],
+        (_, result) => {
+          resolve(result);
+        },
+        (_, error) => {
+          reject(error);
+        },
+      );
+    });
+  });
+
+  return promise;
+}
+
+export function fetchPlaces() {
+  const promise = new Promise((resolve, reject) => {
+    database.transaction(tx => {
+      tx.executeSql(
+        'SELECT * FROM places',
+        [],
+        (_, result) => {
+          const places = [];
+
+          for (const dp of result.rows._array) {
+            places.push(
+              new Place(
+                dp.title,
+                dp.imageUri,
+                {
+                  address: dp.address,
+                  lat: dp.lat,
+                  lng: dp.lng,
+                },
+                dp.id,
+              ),
+            );
+          }
+          resolve(places);
         },
         (_, error) => {
           reject(error);
