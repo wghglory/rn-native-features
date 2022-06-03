77b36b814b938702de9b5aff3f0abacbb212482b
Guanghui Wang <guanghui-wang@foxmail.com>
2022-06-03 21:53:00 +0800

    feat: list of places

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -16,6 +16,7 @@ function PlacesList({ places }) {
 
   return (
     <FlatList
+      style={styles.list}
       data={places}
       keyExtractor={item => item.id}
       renderItem={({ item }) => <PlaceItem place={item} />}
@@ -26,6 +27,9 @@ function PlacesList({ places }) {
 export default PlacesList;
 
 const styles = StyleSheet.create({
+  list: {
+    margin: 24,
+  },
   fallbackContainer: {
     flex: 1,
     justifyContent: 'center',
